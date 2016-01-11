2239eda2473a3b9de384276b71372ad9f243673f
Sascha Arnold <sascha.arnold@dfki.de>
2016-01-11 15:09:27 +0100

    added boost serialization based opaque auto generation plugin for orogen

---
+++ rock/orogen_plugin.rb
@@ -0,0 +1,65 @@
+
+class OroGen::Gen::RTT_CPP::Typekit
+
+    def wrapper_type_name_for type
+        type = type.to_str
+        type = Typelib::Type.normalize_typename(type)
+        path = Typelib.split_typename(type)
+        path.map! do |p|
+            p.gsub(/[<>\[\], \/]/, '_')
+        end
+        "/" + path.join("/") + "_w"
+    end
+
+    def opaque_autogen type, options = Hash.new
+        options = Kernel.validate_options options,
+            :include => [],
+            :includes => [],
+            :type => :boost_serialization
+
+        # Check if type is already available
+        begin
+            t = find_type(type)
+            if !t.nil?
+                raise ArgumentError, "Type #{type} is already defined."
+            end
+        rescue Typelib::NotFound
+        end
+
+        orogen_install_path = File.expand_path(File.join('..'), File.dirname(__FILE__))
+
+        if options[:type] == :boost_serialization
+            options.delete(:type)
+            options[:needs_copy] = true
+
+            # extend includes
+            if options[:includes].respond_to?(:to_str)
+                options[:includes] = [options[:includes]]
+            end
+            options[:includes].push("boost/archive/polymorphic_binary_iarchive.hpp")
+            options[:includes].push("boost/archive/polymorphic_binary_oarchive.hpp")
+            options[:includes].push("envire_core/typekit/BinaryBufferHelper.hpp")
+
+            intermediate_type = wrapper_type_name_for type
+
+            # create and load intermediate wrapper type
+            begin
+                t = find_type(intermediate_type)
+            rescue Typelib::NotFound
+                auto_gen_wrapper_code = Generation.render_template orogen_install_path, 'templates', 'auto_gen_types.hpp', binding
+                path = Generation.save_automatic 'typekit', 'types', self.name, "wrappers", "#{Typelib.basename(intermediate_type)}.hpp", auto_gen_wrapper_code
+                self.load(path, false)
+            end
+
+            # create c++ convertion code from template
+            auto_gen_wrapper_code = Generation.render_template orogen_install_path, 'templates', 'opaque_convertions_boost_serialization.cpp', binding
+
+            # register opaque type
+            opaque_type(type, intermediate_type, options) {auto_gen_wrapper_code}
+        else
+            raise ArgumentError, "Cannot generate opaque, #{options[:type]} is an unknown serialization type!"
+        end
+
+    end
+
+end
---
+++ rock/typekit/templates/opaque_convertions_boost_serialization.cpp
@@ -0,0 +1,21 @@
+<%
+# Generate boost serialization convertions
+%>
+void orogen_typekits::toIntermediate(<%= Typelib::Type.normalize_cxxname(intermediate_type) %>& intermediate, <%= Typelib::Type.normalize_cxxname(type) %> const& real_type)
+{
+    intermediate.binary.reserve(1000000);
+    BinaryOutputBuffer buffer(&intermediate.binary);
+    std::ostream ostream(&buffer);
+    boost::archive::polymorphic_binary_oarchive oa(ostream);
+
+    oa << real_type;
+}
+
+void orogen_typekits::fromIntermediate(<%= Typelib::Type.normalize_cxxname(type) %>& real_type, <%= Typelib::Type.normalize_cxxname(intermediate_type) %> const& intermediate)
+{
+    BinaryInputBuffer buffer(intermediate.binary);
+    std::istream istream(&buffer);
+    boost::archive::polymorphic_binary_iarchive ia(istream);
+
+    ia >> real_type;
+}
