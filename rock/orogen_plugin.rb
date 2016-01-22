130afaeac9dea62c7cd4eb825003826042211648
Sascha Arnold <sascha.arnold@dfki.de>
2016-01-22 18:20:36 +0100

    improved envire serialization, the embedded type is now handled using typelib

--- rock/orogen_plugin.rb
+++ rock/orogen_plugin.rb
@@ -25,7 +25,8 @@ class OroGen::Gen::RTT_CPP::Typekit
         options = Kernel.validate_options options,
             :include => [],
             :includes => [],
-            :type => :boost_serialization
+            :type => :boost_serialization,
+            :embedded_type => nil
 
         # Check if type is already available
         begin
@@ -39,8 +40,6 @@ class OroGen::Gen::RTT_CPP::Typekit
         orogen_install_path = File.expand_path(File.join('..'), File.dirname(__FILE__))
 
         if options[:type] == :boost_serialization
-            options.delete(:type)
-            options[:needs_copy] = true
 
             # extend includes
             options = extend_includes_by_boost_serialization_includes options
@@ -60,18 +59,34 @@ class OroGen::Gen::RTT_CPP::Typekit
             auto_gen_wrapper_code = Generation.render_template orogen_install_path, 'templates', 'opaque_convertions_boost_serialization.cpp', binding
 
             # register opaque type
+            options.delete(:type)
+            options.delete(:embedded_type)
+            options[:needs_copy] = true
             opaque_type(type, intermediate_type, options) {auto_gen_wrapper_code}
 
         elsif options[:type] == :envire_serialization
-            options.delete(:type)
-            options[:needs_copy] = true
+
+            # check if embedded type is set
+            if not options[:embedded_type].respond_to?(:to_str) or options[:embedded_type].empty?
+                raise ArgumentError, "Cannot generate opaque for type #{type} without a valid type name for the embedded type of the envire::core::Item. (E.g. :embedded_type => '/pcl/PCLPointCloud2')"
+            end
+
+            begin
+                # try to find embedded type
+                embedded_type = find_type options[:embedded_type]
+            rescue Typelib::NotFound
+                RTT_CPP.info "Couldn't find typelib type for embedded type #{options[:embedded_type]}, trying to generate boost serialization based opaque type."
+                # generate boost serialization based opaque type if not available
+                opaque_autogen options[:embedded_type], :includes => options[:includes], :include => options[:include], :type => :boost_serialization
+                embedded_type = find_type options[:embedded_type]
+            end
 
             # extend includes
             options = extend_includes_by_boost_serialization_includes options
 
             intermediate_type = wrapper_type_name_for type
 
-            # create and load intermediate wrapper type
+            #create and load intermediate wrapper type
             begin
                 t = find_type(intermediate_type)
             rescue Typelib::NotFound
@@ -84,6 +99,9 @@ class OroGen::Gen::RTT_CPP::Typekit
             auto_gen_wrapper_code = Generation.render_template orogen_install_path, 'templates', 'opaque_convertions_envire_serialization.cpp', binding
 
             # register opaque type
+            options.delete(:type)
+            options.delete(:embedded_type)
+            options[:needs_copy] = true
             opaque_type(type, intermediate_type, options) {auto_gen_wrapper_code}
 
         else
--- rock/typekit/templates/auto_gen_envire_types.hpp
+++ rock/typekit/templates/auto_gen_envire_types.hpp
@@ -3,6 +3,14 @@
 <% target_namespace = Typelib.namespace(intermediate_type)
    target_basename  = Typelib.basename(intermediate_type)
    target_basename.gsub!('/', '::')
+   if embedded_type.opaque?
+        embedded_type_intermediate = intermediate_type_for embedded_type
+        embedded_type_cxx = Typelib::Type.normalize_cxxname(embedded_type_intermediate.name)
+        embedded_type_includes = include_for_type(embedded_type_intermediate)
+   else
+        embedded_type_cxx = Typelib::Type.normalize_cxxname(embedded_type.name)
+        embedded_type_includes = include_for_type(embedded_type)
+   end
 %>
 
 #ifndef _OROGEN_WRAPPER_TYPES_<%= target_basename.upcase %>_HPP
@@ -13,13 +21,20 @@
 #include <stdint.h>
 #include <base/Time.hpp>
 
+<%
+embedded_type_includes.each do |include|
+%>
+#include <<%= include %>>
+<% end %>
+
 <%= Generation.adapt_namespace('/', target_namespace) %>
 
 struct <%= target_basename %>
 {
     base::Time time;
+    uint8_t uuid[16];
     std::string frame;
-    std::vector<uint8_t> map_data;
+    <%= embedded_type_cxx %> data;
 };
 
 <%= Generation.adapt_namespace(target_namespace, '/') %>
